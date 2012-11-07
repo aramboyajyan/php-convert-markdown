07141f0b2f85a6af33520fc05056a6094cee3111
topsitemakers <support@topsitemakers.com>
2012-11-07 20:28:17 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# Markdown processor
+
+Simple markdown processor which allows you to convert any text into HTML using Markdown syntax.
+
+This script should be used only for internal purposes, such as for content writers, and should never be accessible for anyone over the internet.  
+It does not perform any security checks and can potentially allow exploits of your server.
+
+### Credits
+
+[Markdown and Extra](http://www.michelf.com/projects/php-markdown/)  
+[Original Markdown](http://daringfireball.net/projects/markdown/)
+
+<hr>
+
+By: [topsitemakers.com](http://www.topsitemakers.com).
---
+++ index.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * @file
+ * Markdown processing script
+ * 
+ * Convert any text into HTML with markdown.
+ *
+ * This script is NOT SECURE.
+ * It should be used only locally and as a tool for content writers.
+ *
+ * Created by: Topsitemakers
+ * http://www.topsitemakers.com/
+ */
+
+// Get markdown
+require 'markdown.php';
+
+if ($_POST) {
+  $output = Markdown($_POST['input']);
+  // Filter out common problematic characters that are usually copied from Word
+  // and other rich text editors. If necessary, these can be returned back with
+  // something like Smarty pants
+  $output = strtr($output, array(
+    '„' => '"',
+    '“' => '"',
+    '”' => '"',
+    '–' => '-',
+    '‘' => "'",
+    '’' => "'",
+  ));
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Markdown processor</title>
+<style type="text/css">
+body {
+  padding: 0;
+  margin: 0;
+}
+.row {
+  float: left;
+  width: 98%;
+  margin: 0 1%;
+}
+textarea {
+  font-family: monospace;
+  font-size: 14px;
+  width: 98%;
+  padding: 1%;
+  margin: 1% 0 0 0;
+  min-height: 250px;
+}
+textarea,
+textarea[disabled=disabled] {
+  background: #FFF;
+}
+input[type=submit] {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 30px;
+  cursor: pointer;
+}
+</style>
+</head>
+<body>
+
+<form action="/playground/markdown/" method="post">
+  
+  <div class="row">
+    <?php if ($_POST): ?>
+    <textarea id="input" name="input"><?php print $_POST['input']; ?></textarea>
+    <?php else: ?>
+    <textarea id="input" name="input"></textarea>
+    <?php endif; ?>
+  </div>
+  
+  <div class="row" style="text-align: center;">
+    <input type="submit" value="Convert">
+  </div>
+
+  <div class="row">
+    <?php if ($_POST): ?>
+    <textarea id="output" name="output" disabled="disabled"><?php print $output; ?></textarea>
+    <?php endif; ?>
+  </div>
+
+</form>
+
+</body>
+</html>
